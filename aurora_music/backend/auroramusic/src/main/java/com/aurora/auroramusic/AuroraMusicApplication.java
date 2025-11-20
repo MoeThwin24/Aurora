7419703d222fd8ed6ff3befc98c3069c4786b5b1
MoeThwin24 <s3967740@student.rmit.edu.au>
2025-11-20 23:47:08 +1100

    Add Spring Boot backend + React frontend structure

---
+++ aurora_music/backend/auroramusic/src/main/java/com/aurora/auroramusic/AuroraMusicApplication.java
@@ -0,0 +1,13 @@
+package com.aurora.auroramusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuroraMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuroraMusicApplication.class, args);
+	}
+
+}
---
+++ aurora_music/backend/auroramusic/src/test/java/com/aurora/auroramusic/AuroraMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurora.auroramusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuroraMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
